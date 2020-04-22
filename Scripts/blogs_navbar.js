8984d92af0a33233008b12757c219efe95bdcb1f
Devan <dbg1709@gmail.com>
2020-04-22 14:01:20 +0200

    Added a Consistent Navigation Metaphor driven by JavaScript.

---
+++ Scripts/blogs_navbar.js
@@ -0,0 +1,21 @@
+const nav_section = document.getElementById("navbar_header");
+
+nav_section.innerHTML = [
+    "<section>",
+    "<img src=\"../../Images/StevenStatLogo.PNG\" alt=\"Steven Stat Logo\" width=\"250\">",
+    "</section>",
+    "<br>",
+    "<nav>",
+    "<section>",
+    "<form align=\"center\">",
+    "<input type=\"button\" value=\"Back\" onclick=\"history.back()\">",
+    "</form>",
+    "<br>",
+    "<a href=\"../../index.html\">Home</a> |",
+    "<a href=\"../statStories\">StatStories</a> |",
+    "<a href=\"../../steven-stat-about.html\">About</a> |",
+    "<a href=\"../../steven-stat-contact.html\">Contact</a> |",
+    "<a href=\"./\">Course Blogs</a>",
+    "</section>",
+    "</nav>"
+].join("\n");
