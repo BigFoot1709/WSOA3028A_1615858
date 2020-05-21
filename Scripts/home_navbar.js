e77990fbb54e3f6894dcd9167afadb3f56af9005
Devan <dbg1709@gmail.com>
2020-05-21 14:48:28 +0200

    Applying CSS to all pages.

--- Scripts/home_navbar.js
+++ Scripts/home_navbar.js
@@ -1,21 +1,16 @@
 const nav_section = document.getElementById("navbar_header");
 
 nav_section.innerHTML = [
-    "<section>",
-    "<img src=\"Images/StevenStatLogo.PNG\" alt=\"Steven Stat Logo\" width=\"250\">",
-    "</section>",
-    "<br>",
+    "<div class=\"container\">",
+    "<img src=\"./Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\">",
     "<nav>",
-    "<section>",
-    "<form align=\"center\">",
-    "<input type=\"button\" value=\"Back\" onclick=\"history.back()\">",
-    "</form>",
-    "<br>",
-    "<a href=\"./index.html\">Home</a> |",
-    "<a href=\"./blogs/statStories\">StatStories</a> |",
-    "<a href=\"./steven-stat-about.html\">About</a> |",
-    "<a href=\"./steven-stat-contact.html\">Contact</a> |",
-    "<a href=\"./blogs/courseBlogs\">Course Blogs</a>",
-    "</section>",
-    "</nav>"
+    "<li><a href=\"./index.html\">Home</a></li>",
+    "<li><a href=\"./blogs/statStories\">StatStories</a></li>",
+    "<li><a href=\"./steven-stat-about.html\">About</a></li>",
+    "<li><a href=\"./steven-stat-contact.html\">Contact</a></li>",
+    "<li><a href=\"./blogs/courseBlogs\">Course Blogs</a></li>",
+    "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
+    "</ul>",
+    "</nav>",
+    "</div>"
 ].join("\n");
