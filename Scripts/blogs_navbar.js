dd7f6bb6a4645f2dbf10f70a75720aa14685349e
Devan <dbg1709@gmail.com>
2020-05-21 16:09:41 +0200

    Fixed up the Blog Pages to look better.

--- Scripts/blogs_navbar.js
+++ Scripts/blogs_navbar.js
@@ -1,16 +1,25 @@
-const nav_section = document.getElementById("navbar_header");
 
-nav_section.innerHTML = [
-    "<div class=\"container\">",
-    "<img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\">",
-    "<nav>",
-    "<li><a href=\"../../index.html\">Home</a></li>",
-    "<li><a href=\"../statStories\">StatStories</a></li>",
-    "<li><a href=\"../../steven-stat-about.html\">About</a></li>",
-    "<li><a href=\"../../steven-stat-contact.html\">Contact</a></li>",
-    "<li><a href=\"./\">Course Blogs</a></li>",
-    "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
-    "</ul>",
-    "</nav>",
-    "</div>"
-].join("\n");
+
+
+
+    const nav_section = document.getElementById("navbar_header");
+
+    nav_section.innerHTML = [
+        "<div class=\"container\">",
+        "<img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\">",
+        "<nav>",
+        "<li><a href=\"../../index.html\">Home</a></li>",
+        "<li><a href=\"../statStories\">StatStories</a></li>",
+        "<li><a href=\"../../steven-stat-about.html\">About</a></li>",
+        "<li><a href=\"../../steven-stat-contact.html\">Contact</a></li>",
+        "<li><a href=\"./\">Course Blogs</a></li>",
+        "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
+        "</ul>",
+        "</nav>",
+        "</div>"
+    ].join("\n");
+
+
+
+
+    
