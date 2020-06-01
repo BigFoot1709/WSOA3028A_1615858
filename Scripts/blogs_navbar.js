a3a36c4578fec01cd5e97dcc159a2f70f1f6e0cc
Devan <dbg1709@gmail.com>
2020-06-01 18:03:31 +0200

    Small Corrections from Assignment 2 Feedback.

--- Scripts/blogs_navbar.js
+++ Scripts/blogs_navbar.js
@@ -1,25 +1,16 @@
+const nav_section = document.getElementById("navbar_header");
 
-
-
-
-    const nav_section = document.getElementById("navbar_header");
-
-    nav_section.innerHTML = [
-        "<div class=\"container\">",
-        "<img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\">",
-        "<nav>",
-        "<li><a href=\"../../index.html\">Home</a></li>",
-        "<li><a href=\"../statStories\">StatStories</a></li>",
-        "<li><a href=\"../../steven-stat-about.html\">About</a></li>",
-        "<li><a href=\"../../steven-stat-contact.html\">Contact</a></li>",
-        "<li><a href=\"./\">Course Blogs</a></li>",
-        "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
-        "</ul>",
-        "</nav>",
-        "</div>"
-    ].join("\n");
-
-
-
-
-    
+nav_section.innerHTML = [
+    "<div class=\"container\">",
+    "<a href=\"../../index.html\"><img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\"></a>",
+    "<nav>",
+    "<li><a href=\"../../index.html\">Home</a></li>",
+    "<li><a href=\"../statStories\">StatStories</a></li>",
+    "<li><a href=\"../../steven-stat-about.html\">About</a></li>",
+    "<li><a href=\"../../steven-stat-contact.html\">Contact Me</a></li>",
+    "<li><a href=\"./\">Course Blogs</a></li>",
+    "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
+    "</ul>",
+    "</nav>",
+    "</div>"
+].join("\n");
--- Scripts/statstories_navbar.js
+++ Scripts/statstories_navbar.js
@@ -2,12 +2,12 @@ const nav_section = document.getElementById("navbar_header");
 
 nav_section.innerHTML = [
     "<div class=\"container\">",
-    "<img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\">",
+    "<a href=\"../../index.html\"><img src=\"../../Images/StevenStatLogo.PNG\" alt=\"logo\" class=\"logo\" width=\"300\"></a>",
     "<nav>",
     "<li><a href=\"../../index.html\">Home</a></li>",
     "<li><a href=\"./\">StatStories</a></li>",
     "<li><a href=\"../../steven-stat-about.html\">About</a></li>",
-    "<li><a href=\"../../steven-stat-contact.html\">Contact</a></li>",
+    "<li><a href=\"../../steven-stat-contact.html\">Contact Me</a></li>",
     "<li><a href=\"../courseBlogs\">Course Blogs</a></li>",
     "<li><input type=\"button\" class=\"back_button\" value=\"Back\" onclick=\"history.back()\"></li>",
     "</ul>",
